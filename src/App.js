7f6ac3251f92668adb0a80cce01eef3793cb88c6
Sachinsharma7398 <sachin@nexgsolution.com>
2022-06-19 11:09:48 +0530

    dump removed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Banner from "./components/banner/Banner";
 import Footer from "./components/footer/Footer";
 import GetInTouch from "./components/getintouch/GetInTouch";
 import Header from "./components/header/Header";
-import Launch from "./components/launch/Launch";
-import Partners from "./components/partners/Partners";
 import Perfomers from "./components/perfomers/Perfomers";
-import Portfolio from "./components/portfolio/Portfolio";
 import Team from "./components/team/Team";
 
 const App = () => {
